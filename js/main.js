c4d2aa1054755d9fef55c2c69276fbe21bf7fc6d
choir27 <richardchoi54@gmail.com>
2022-04-19 15:26:03 -0400

    Add Hand Totals, check win/lose conditions, fix convertNumber function Allows replay after showing hand, add html/css start localStorage values :wq

--- js/main.js
+++ js/main.js
@@ -16,8 +16,22 @@ fetch(deck)
   DOM('.button').addEventListener('click',drawFour)
   })
 
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
   function drawFour(){
+
+    DOM('#theirTotal').classList.add('hidden')
+    DOM('#OppCards1').src = 'img/back.jpg'
+    removeAllChildNodes(DOM('#OppHand'))
+    removeAllChildNodes(DOM('#yourHand'))
+    DOM('#result').innerText = 'Result'
+    oppVal = []
+    yourVal = []
+
     DOM('#show').classList.remove('hidden')
     DOM('#bet').classList.remove('hidden')
 
@@ -27,75 +41,137 @@ fetch(drawFour)
   .then(data=>{
    opponentCards = [data.cards[0].images.png, data.cards[1].images.png]
    let yourCards = [data.cards[2].images.png, data.cards[3].images.png]
+
+   localStorage.setItem('oppImages',opponentCards)
+   localStorage.setItem('yourImages', yourCards)
+
+
   DOM('#yourCards').src = yourCards[0]
   DOM('#yourCards1').src = yourCards[1]
+  DOM('#OppCards').src = opponentCards[0]
 
   oppVal.push(data.cards[0].value, data.cards[1].value)
   yourVal.push(data.cards[2].value, data.cards[3].value)
+  convertNumber(yourVal)
+  DOM('.button').classList.add('hidden')
+  getSum()
 
-  DOM('.button').remove()
-
-  
   DOM('#show').addEventListener('click',showCards)
   DOM('#bet').addEventListener('click',bet)
   })
   }
 
 function showCards(){
-DOM('#OppCards').src = opponentCards[0]
 DOM('#OppCards1').src = opponentCards[1]
-DOM('#show').remove()
-DOM('#bet').remove()
-getSum()
+DOM('#show').classList.add('hidden')
+DOM('#bet').classList.add('hidden')
+DOM('#theirTotal').classList.remove('hidden')
+getFinalSum()
+DOM('.button').classList.remove('hidden')
+
+
 }
 
 function bet(){
-let drawOne = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-fetch(drawOne)
+let drawTwo= `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+fetch(drawTwo)
   .then(res=>res.json())
   .then(data=>{
+    
   let newCard = document.createElement('img')
   DOM('#yourHand').appendChild(newCard)
   newCard.src = data.cards[0].images.png
   yourVal.push(data.cards[0].value)
+  convertNumber(yourVal)
+  localStorage.setItem('newCard',data.cards[0].images.png)
+  getSum()
+
+  let newOppCard = document.createElement('img')
+  DOM('#OppHand').appendChild(newOppCard)
+  newOppCard.src = data.cards[1].images.png
+  oppVal.push(data.cards[1].value)
+  convertNumber(oppVal)
+  localStorage.setItem('newCard',data.cards[0].images.png)
+  getSum()
+
 })
 }
 
-function convertNumber(){
-  yourVal.forEach(ele=>{
-    ele = ele.toLowerCase()
-    console.log(ele)
-    if(ele==='jack'||ele==='queen'||ele==='king'){
-      return 10
-    }else if(ele==='ace'){
-      return 11
-    }else{
-      return Number(ele)
-    }
-  })
-  oppVal.forEach(ele=>{
+
+function convertNumber(arr){
+  return arr.map(ele=>{
     ele = ele.toLowerCase()
     if(ele==='jack'||ele==='queen'||ele==='king'){
       return 10
     }else if(ele==='ace'){
-      return 11
+     return 11
     }else{
       return Number(ele)
     }
   })
 }
 
+
 function getSum(){
-  DOM('#yourTotal').classList.remove('hidden')
-  DOM('#theirTotal').classList.remove('hidden')
-  convertNumber()
-console.log(yourVal)
-//   let yourSum = yourVal.reduce((a,b)=>{
-// return a+b
-//   },0)
-//   DOM('#yourTotal').innerText = yourSum
-//   let theirSum = oppVal.reduce((a,b)=>{
-// return a+b
-//   },0)
-  // DOM('#theirTotal').innerText = theirSum
+  
+  let yourConverted = convertNumber(yourVal) 
+  let oppConverted = convertNumber(oppVal)
+
+  let yourSum = yourConverted.reduce((a,b)=>{
+return a+b
+  },0)
+  DOM('#yourTotal').innerText = yourSum
+  localStorage.setItem('getYourSum',yourSum)
+
+
+  let theirSum = oppConverted.reduce((a,b)=>{
+return a+b
+  },0)
+
+  DOM('#theirTotal').innerText = theirSum
+  localStorage.setItem('getTheirSum',yourSum)
+
+  if(yourSum>21){
+    DOM('#result').innerText = "You Lose!"
+    DOM('.button').classList.remove('hidden')
+  }else if(theirSum>21){
+    DOM('#result').innerText = "You Win!"
+    DOM('.button').classList.remove('hidden')
+  }
+ 
 }
+
+function getFinalSum(){
+  let yourConverted = convertNumber(yourVal) 
+  let oppConverted = convertNumber(oppVal)
+
+  let yourSum = yourConverted.reduce((a,b)=>{
+return a+b
+  },0)
+  DOM('#yourTotal').innerText = yourSum
+
+
+  let theirSum = oppConverted.reduce((a,b)=>{
+return a+b
+  },0)
+
+  DOM('#theirTotal').innerText = theirSum
+  if(yourSum>21){
+    DOM('#result').innerText = "You Lose!"
+    DOM('.button').classList.remove('hidden')
+  }else if(theirSum>21){
+    DOM('#result').innerText = "You Win!"
+    DOM('.button').classList.remove('hidden')
+  }else if(yourSum>theirSum){
+    DOM('#result').innerText = "You Win!"
+    DOM('.button').classList.remove('hidden')
+  }else if(theirSum>yourSum){
+    DOM('#result').innerText = "You Lose!"
+    DOM('.button').classList.remove('hidden')
+  }else if(theirSum===yourSum){
+    DOM('#result').innerText = "It is a Tie!"
+    DOM('.button').classList.remove('hidden')
+
+  }
+}
+
